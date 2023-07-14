cebd58dbdab423d0e6e8671895aac9fc991210e1
matias <matias901zhang@gmail.com>
2023-07-14 16:47:51 +0100

    fixed merge conflict in element/button/index

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -1,21 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 
-<<<<<<< HEAD
-const Button = ({ onClick, variant = "primary", className, children }) => {
-=======
 const Button = ({ variant = "primary", className, children, icon, onClick, size = "md" }) => {
->>>>>>> 20d77caf4d0ef5c571294208dbfcd7625c514883
   return (
     <button
-      onClick={onClick}
       className={classNames(
         {
           "bg-gradientPrimary hover:bg-gradientSecondary bg-origin-border": variant === "primary",
           "bg-transparent hover:text-gray-400": variant === "secondary",
           "bg-transparent border-white hover:border-gray-400": variant === "outline",
-          "py-2.5 px-5": size == "md",
-          "py-1.5 px-4": size == "sm",
+          "py-2.5 px-5": size === "md",
+          "py-1.5 px-4": size === "sm",
         },
         "flex flex-row gap-2 text-white rounded-lg text-md sm:text-xl items-center border-2 border-transparent",
         className
