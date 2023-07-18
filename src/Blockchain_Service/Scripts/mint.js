4863905b74570391e34b378e97c60027eb8cda3a
matias <matias901zhang@gmail.com>
2023-07-18 16:44:11 +0100

    created the minting and approval integrations

--- src/Blockchain_Service/Scripts/mint.js
+++ src/Blockchain_Service/Scripts/mint.js
@@ -1,33 +1,34 @@
-import { Contract, ethers } from "ethers";
+import { ethers } from "ethers";
 import CombinedWeaveAbi from "../../lib/combinedAbi";
-import SDK_NODE from "weavedb-sdk-node"
-const provider = new ethers.JsonRpcProvider(process.env.EVM_RPC_URL)
-const contractTxId = process.env.NEXT_PUBLIC_WEAVEDB_CONTRACT_TX_ID
-const nftContractAddr = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS
+import { NftContext } from "../../store/NftContext";
+import { useContext } from "react";
+const provider = new ethers.providers.JsonRpcProvider(process.env.EVM_RPC_URL);
+const nftContractAddr = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS;
 
-export default async function handler(req, res) {
-    try {
-      const params = JSON.parse(req.body)
-      const tokenID = params.query[0].tokenID
-      const owner = await new Contract(nftContractAddr, CombinedWeaveAbi, provider).ownerOf(
-        tokenID
-      )
-   
-      const sdk = new SDK_NODE({
-        contractTxId,
-      })
-      await sdk.init()
-   
-      const tx = await sdk.relay(params.jobID, params, owner, {
-        jobID: params.jobID,
-        privateKey: process.env.RELAYER_PRIVATEKEY,
-      })
-      const result = await tx.getResult()
-      res.status(200).json({ success: true, result })
-    } catch (error) {
-      res.status(200).json({
-        success: false,
-        error,
-      })
-    }
+export default async function Minting(req, res) {
+  // Accessing context using useContext hook
+  const { setTokenID } = useContext(NftContext);
+
+  try {
+    
+    // Connect to the NFT contract
+    const contract = new ethers.Contract(nftContractAddr, CombinedWeaveAbi, provider);
+
+    // Call the mint function
+    const mintTx = await contract.mint();
+    const receipt = await mintTx.wait();
+
+    // Retrieve the emitted token ID from the transaction receipt
+    const tokenID = receipt.events[0].args.tokenID.toNumber();
+
+    // Update the context store with the token ID
+    setTokenID(tokenID); // Assuming setTokenID is the function to update the token ID in your context
+
+    res.status(200).json({ success: true, tokenID: tokenID });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      error,
+    });
   }
+}
